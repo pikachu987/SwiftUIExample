0e6ba7130bfc46cd8525c79e04fc4e4834d18de0
pikachu987 <pikachu77769@gmail.com>
2021-07-27 18:01:13 +0900

    프리뷰 테스트

--- SwiftUIExample/ContentView.swift
+++ SwiftUIExample/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                .padding()
+                .font(.largeTitle)
+                .foregroundColor(.orange)
+            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+                .previewDisplayName("iPhone 11")
+            
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+                .previewDisplayName("iPhone 11")
+                .environment(\.colorScheme, .dark)
+
+            ContentView()
+                .previewDevice("iPhone SE (2nd generation)")
+                .previewDisplayName("iPhone SE")
+        }
     }
 }
