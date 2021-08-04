416499c3115aec1bad910f6111f86debb2e5ec65
pikachu987 <pikachu77769@gmail.com>
2021-08-04 16:32:34 +0900

    Picker, Divider

--- SwiftUIExample/ContentView.swift
+++ SwiftUIExample/ContentView.swift
@@ -6,33 +6,51 @@
 //
 
 import SwiftUI
-import Combine
-
-class DemoData: ObservableObject {
-    @Published var userCount = 0
-    @Published var currentUser = ""
-    
-    init() {
-        
-    }
-    
-    func updateData() {
-        
-    }
-}
 
 struct ContentView: View {
-    @ObservedObject var demoData: DemoData
+    var colors: [Color] = [.black, .red, .green, .blue]
+    var colornames = ["Black", "Red", "Green", "Blue"]
     
+    @State private var colorIndex: Int = 0
+    @State private var rotation: Double = 0
+    @State private var text: String = "Welcome to SwiftUI"
+
     var body: some View {
-        Text("\(demoData.currentUser), count: \(demoData.userCount)")
+        VStack {
+            Spacer()
+            
+            Text(text)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .rotationEffect(.degrees(rotation))
+                .animation(.easeInOut(duration: 5))
+                .foregroundColor(colors[colorIndex])
+            
+            Spacer()
+            Divider()
+            
+            Slider(value: $rotation, in: 0...360, step: 0.1)
+                .padding()
+            
+            TextField("Enter text", text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Picker(selection: $colorIndex, label: Text("Color")) {
+                ForEach(0..<colornames.count) {
+                    Text(colornames[$0])
+                        .foregroundColor(colors[$0])
+                }
+            }
+            .padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            ContentView(demoData: DemoData())
+            ContentView()
                 .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
                 .previewDisplayName("iPhone 11")
         }
