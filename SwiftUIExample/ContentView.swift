f7d6dc79fb41c90d60891e72cb1c4a5dc13578a2
pikachu987 <pikachu77769@gmail.com>
2021-08-06 15:36:52 +0900

    dynamic List

--- SwiftUIExample/ContentView.swift
+++ SwiftUIExample/ContentView.swift
@@ -7,20 +7,24 @@
 
 import SwiftUI
 
+struct ToDoItem: Identifiable {
+    var id = UUID()
+    var task: String
+    var imageName: String
+}
+
+var listData: [ToDoItem] = [
+    ToDoItem(task: "List Test 1", imageName: "trash.cirlce.fill"),
+    ToDoItem(task: "List Test 2", imageName: "person.2.fill"),
+    ToDoItem(task: "List Test 3", imageName: "car.fill"),
+]
+
 struct ContentView: View {
     var body: some View {
-        List {
+        List(listData) { item in
             HStack {
-                Image(systemName: "trash.circle.fill")
-                Text("List Test 1")
-            }
-            HStack {
-                Image(systemName: "person.2.fill")
-                Text("List Test 2")
-            }
-            HStack {
-                Image(systemName: "car.fill")
-                Text("List Test 3")
+                Image(systemName: item.imageName)
+                Text(item.task)
             }
         }
     }
