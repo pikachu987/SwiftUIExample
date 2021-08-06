4d73f187d881969e38896cb850a272d258627c67
pikachu987 <pikachu77769@gmail.com>
2021-08-06 15:33:12 +0900

    List

--- SwiftUIExample/ContentView.swift
+++ SwiftUIExample/ContentView.swift
@@ -9,62 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack(alignment: .custom) {
-            Rectangle()
-                .foregroundColor(.green)
-                .alignmentGuide(HorizontalAlignment.customHorizontal, computeValue: { dimension in
-//                    dimension[.trailing]
-                    dimension[.leading]
-                })
-                .alignmentGuide(VerticalAlignment.customVertical, computeValue: { dimension in
-                    dimension[.bottom]
-                })
-                .frame(width: 100, height: 100)
-            Rectangle()
-                .foregroundColor(.red)
-                .alignmentGuide(HorizontalAlignment.customHorizontal, computeValue: { dimension in
-//                    dimension[HorizontalAlignment.center]
-                    dimension[HorizontalAlignment.trailing]
-                })
-                .alignmentGuide(VerticalAlignment.customVertical, computeValue: { dimension in
-//                    dimension[.top]
-                    dimension[VerticalAlignment.center]
-                })
-                .frame(width: 100, height: 100)
-            Circle()
-                .foregroundColor(.orange)
-                .alignmentGuide(HorizontalAlignment.customHorizontal, computeValue: { dimension in
-                    dimension[.leading]
-                })
-                .alignmentGuide(VerticalAlignment.customVertical, computeValue: { dimension in
-//                    dimension[.bottom]
-                    dimension[.top]
-                })
-                .frame(width: 100, height: 100)
+        List {
+            Text("List Test 1")
+            Text("List Test 2")
+            Text("List Test 3")
+            Text("List Test 4")
+            Text("List Test 5")
         }
     }
-}
-
-extension HorizontalAlignment {
-    private enum CustomHorizontal: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[HorizontalAlignment.center]
-        }
-    }
-    static let customHorizontal = HorizontalAlignment(CustomHorizontal.self)
-}
-
-extension VerticalAlignment {
-    private enum CustomVertical: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[VerticalAlignment.center]
-        }
-    }
-    static let customVertical = VerticalAlignment(CustomVertical.self)
-}
-
-extension Alignment {
-    static let custom = Alignment(horizontal: HorizontalAlignment.customHorizontal, vertical: VerticalAlignment.customVertical)
 }
 
 struct ContentView_Previews: PreviewProvider {
