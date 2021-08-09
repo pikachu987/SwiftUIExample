cad162d8199a4565f3ef0620f822faeb425eecd8
pikachu987 <pikachu77769@gmail.com>
2021-08-09 15:14:07 +0900

    Custom Path

--- SwiftUIExample/ContentView.swift
+++ SwiftUIExample/ContentView.swift
@@ -11,20 +11,23 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Path { path in
-                path.move(to: .init(x: 10, y: 0))
-                path.addLine(to: CGPoint(x: 10, y: 350))
-                path.addLine(to: CGPoint(x: 300, y: 300))
-                path.closeSubpath()
-            }
-            
-            Path { path in
-                path.move(to: .init(x: 10, y: 0))
-                path.addLine(to: CGPoint(x: 10, y: 350))
-                path.addLine(to: CGPoint(x: 300, y: 300))
-            }
-            .fill(Color.blue)
+            MyShape()
+                .fill(Color.red)
+                .frame(width: 360, height: 350)
         }
+    }
+}
+
+struct MyShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: .init(x: rect.minX, y: rect.minY))
+        path.addQuadCurve(to: .init(x: rect.minX, y: rect.maxY), control: .init(x: rect.midX, y: rect.midY))
+        path.addLine(to: .init(x: rect.minX, y: rect.maxY))
+        path.addLine(to: .init(x: rect.maxX, y: rect.maxY))
+        path.closeSubpath()
+        return path
     }
 }
 
