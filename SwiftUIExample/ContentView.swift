e2c9e55de31fd1abfb59341d62ca11e8d65cd63c
pikachu987 <pikachu77769@gmail.com>
2021-08-09 15:10:29 +0900

    Path Test

--- SwiftUIExample/ContentView.swift
+++ SwiftUIExample/ContentView.swift
@@ -11,33 +11,19 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Rectangle()
-                .frame(width: 100, height: 100)
+            Path { path in
+                path.move(to: .init(x: 10, y: 0))
+                path.addLine(to: CGPoint(x: 10, y: 350))
+                path.addLine(to: CGPoint(x: 300, y: 300))
+                path.closeSubpath()
+            }
             
-            Circle()
-                .fill(Color.red)
-                .frame(width: 250, height: 100)
-            
-            Capsule()
-                .stroke(lineWidth: 10)
-                .foregroundColor(.blue)
-                .frame(width: 200, height: 100)
-            
-            RoundedRectangle(cornerRadius: 12, style: .continuous)
-                .stroke(style: StrokeStyle(lineWidth: 4, lineCap: .round, lineJoin: .bevel, dash: [10]))
-                .foregroundColor(.blue)
-                .frame(width: 200, height: 100)
-            
-            Ellipse()
-                .stroke(style: StrokeStyle(lineWidth: 20, dash: [10, 5, 2], dashPhase: 10))
-                .foregroundColor(.orange)
-                .frame(width: 250, height: 100)
-            
-            
-            Ellipse()
-                .fill(Color.red)
-                .overlay(Ellipse().stroke(Color.blue, lineWidth: 4).frame(width: 50, height: 30))
-                .frame(width: 250, height: 100)
+            Path { path in
+                path.move(to: .init(x: 10, y: 0))
+                path.addLine(to: CGPoint(x: 10, y: 350))
+                path.addLine(to: CGPoint(x: 300, y: 300))
+            }
+            .fill(Color.blue)
         }
     }
 }
